a10ee2fefd93aba291c6eb1c096eb7481e6e146d
kayzersozee <63726574+kayzersozee@users.noreply.github.com>
2021-02-06 10:02:24 +0100

    added Themeprovider as global theme in app component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,48 @@
 import Components from "./Components/";
 import GlobalStyle from "./GlobalStyle";
+import { ThemeProvider, createMuiTheme } from "@material-ui/core/styles";
 
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: "#161a1dff",
+    },
+    secondary: {
+      main: "#00b4d8ff",
+    },
+  }, // h1 - h4 is styling for about me components
+  typography: {
+    fontFamily: "Times New Roman",
+
+    h1: {
+      fontSize: "2.5rem",
+      letterSpacing: "1.5px",
+      marginTop: "3vh",
+      marginBottom: "1vh",
+    },
+    h6: {
+      fontSize: "1.5rem",
+      letterSpacing: "0.01em",
+      marginTop: "10%",
+    },
+    body2: {
+      fontSize: "1.5rem",
+      letterSpacing: "0.01em",
+      marginTop: "15px",
+    },
+    h4: {
+      marginTop: "10%",
+    },
+    // h1 - h4 is styling for about me components
+    h3: {},
+  },
+});
 function App() {
   return (
-    <>
+    <ThemeProvider theme={theme}>
       <Components />
       <GlobalStyle />
-    </>
+    </ThemeProvider>
   );
 }
 
--- src/Components/Main/index.js
+++ src/Components/Main/index.js
@@ -4,47 +4,7 @@ import Projects from "./Projects";
 import About from "./About";
 import { Switch, Route } from "react-router-dom";
 import styled from "styled-components";
-import { ThemeProvider, createMuiTheme } from "@material-ui/core/styles";
 
-const theme = createMuiTheme({
-  palette: {
-    primary: {
-      main: "#161a1dff",
-      dark: "#fffffcff",
-    },
-    secondary: {
-      main: "#00b4d8ff",
-      dark: "#0b090aff",
-    },
-  }, // h1 - h4 is styling for about me components
-  typography: {
-    h1: {
-      fontSize: "2.5rem",
-      letterSpacing: "1.5px",
-      marginTop: "3vh",
-      marginBottom: '1vh',
-
-    },
-    h6: {
-      fontSize: "1.5rem",
-      letterSpacing: "0.01em",
-      marginTop: "10%",
-    },
-    body2: {
-      fontSize: "1.5rem",
-      letterSpacing: "0.01em",
-      marginTop: "15px",
-    },
-    h4: {
-      marginTop: "10%",
-    },
-    // h1 - h4 is styling for about me components
-    h3: {
-      
-    }
-  },
-
-});
 
 // om jag vill göra themet till dark använd propen type: 'dark'
 // skapa en if sats till en handlechange knapp och ändra type när onClick
@@ -60,7 +20,7 @@ const StyledMain = styled.main`
 
 function Index() {
   return (
-    <ThemeProvider theme={theme}>
+   
       <StyledMain>
         <Switch>
           <Route exact path="/">
@@ -80,7 +40,7 @@ function Index() {
           </Route>
         </Switch>
       </StyledMain>
-    </ThemeProvider>
+    
   );
 }
 
